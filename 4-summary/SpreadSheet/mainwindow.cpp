d3808797f4ad1564bf97b4b002aa845d59c3acb9
Wind <18575131670@163.com>
2019-06-27 01:26:40 +0800

    菜单栏完成一半，另外图标的名字必须使用英文

--- 4-summary/SpreadSheet/mainwindow.cpp
+++ 4-summary/SpreadSheet/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include<QtGui>
+#include <QtGui>
 #include "spreadsheet.h"
 #include "mainwindow.h"
 
@@ -8,16 +8,44 @@ MainWindow::MainWindow()
 
     setCentralWidget(spreadsheet);
 
+    newAction = new QAction(tr("&New"), this);
+    newAction->setIcon(QIcon(":/icons/new.png"));
+    newAction->setStatusTip(tr("Create a new file"));
+    newAction->setShortcut(QKeySequence::New);
+
+    exitAction = new QAction(tr("&Exit"), this);
+    exitAction->setStatusTip(tr("Exit the application"));
+    exitAction->setShortcut(tr("Ctrl+Q"));
+    connect(exitAction, SIGNAL(triggered(bool)), this, SLOT(close()));
+
     //菜单部分
-    menuBar()->addMenu(tr("&File"));
+    //文件
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(newAction);
+    fileMenu->addAction(exitAction);
     menuBar()->addMenu(tr("&Edit"));
     menuBar()->addMenu(tr("&Tools"));
     menuBar()->addMenu(tr("&Options"));
-    menuBar()->addMenu(tr("&Help"));
+
+    //帮助
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+
+
     //状态栏
     locationLabel = new QLabel(tr(" W999 "));
     locationLabel->setAlignment(Qt::AlignCenter);
     locationLabel->setMinimumSize(locationLabel->sizeHint());
+    //创建并设置动作
+    aboutAction = new QAction(tr("&About"), this);
+    aboutAction->setStatusTip(tr("About SpreadSheet"));
+
+    aboutQtAction = new QAction(tr("About &Qt"), this);
+    aboutQtAction->setStatusTip(tr("About Qt"));
+    connect(aboutQtAction, SIGNAL(triggered(bool)), qApp, SLOT(aboutQt()));
+
+    helpMenu->addAction(aboutAction);
+    helpMenu->addAction(aboutQtAction);
+
 
     formulaLabel = new QLabel;
     formulaLabel->setIndent(3);
--- 4-summary/SpreadSheet/mainwindow.h
+++ 4-summary/SpreadSheet/mainwindow.h
@@ -2,9 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+class QAction;
 class Spreadsheet;
 class QLabel;
+
 class MainWindow : public QMainWindow
 {
 public:
@@ -14,6 +15,18 @@ private:
     Spreadsheet *spreadsheet;
     QLabel *locationLabel;
     QLabel *formulaLabel;
+
+    QMenu *fileMenu;
+
+
+
+    QAction* newAction;
+    QAction* exitAction;
+
+
+    QMenu *helpMenu;
+    QAction *aboutAction;
+    QAction *aboutQtAction;
 };
 
 #endif // MAINWINDOW_H
