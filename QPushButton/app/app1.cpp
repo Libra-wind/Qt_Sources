606d1db992c3c58d9e592a8645e9e21e2558c08f
Wind <18575131670@163.com>
2019-07-10 22:46:05 +0800

    QPushButton

--- QPushButton/app/app1.cpp
+++ QPushButton/app/app1.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     QWidget w;
     w.resize(200,200);
+#if 0
     QPushButton *bt1 = new QPushButton("test1", &w);
     bt1->setGeometry(0,0,80,60);
     bt1->setFlat(true);
@@ -16,6 +17,10 @@ int main(int argc, char* argv[])
     bt2->setDefault(true);
     qDebug() << bt1->isDefault() << endl;
     qDebug() << bt2->isDefault() << endl;
+#endif
+    QPushButton *bt1 = new QPushButton("Popup Button", &w);
+    QMenu *item =
+
     return app.exec();
 }
 
